82e71d2d11e98ad077f49bcc11dbf8fcbeb47d3d
Fiona Rae <cecerae@comcast.net>
2020-01-31 07:38:55 -0500

    Did stuff

---
+++ github/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  github
+//
+//  Created by Fiona Rae (student LM) on 1/31/20.
+//  Copyright © 2020 Fiona Rae (student LM). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("What's up, World?")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
